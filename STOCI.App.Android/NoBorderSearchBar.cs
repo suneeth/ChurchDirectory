6faf5cd017f6dedd6a029d9837fd3039e96e69e6
Suneeth Varghese <suneethvarghese@Suneeths-MacBook-Pro.local>
2018-12-24 11:48:35 -0500

    Initial commit

---
+++ STOCI.App.Android/NoBorderSearchBar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Android.Widget;
+using STOCI.App.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("STOCI.App")]
+[assembly: ExportEffect(typeof(RemoveBorderEffect), "RemoveBorderEffect")]
+namespace STOCI.App.Droid
+{
+    public class RemoveBorderEffect:PlatformEffect
+    {
+         
+
+
+        protected override void OnAttached()
+        {
+            //var searchView = Control as SearchView;
+
+            //int searchPlateId = searchView.Context.Resources.GetIdentifier("android:id/search_plate", null, null);
+            //Android.Views.View searchPlateView = searchView.FindViewById(searchPlateId);
+            //searchPlateView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
---
+++ STOCI.App.iOS/NoBorderSearchBarControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using STOCI.App.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName("STOCI.App")]
+[assembly: ExportEffect(typeof(RemoveBorderEffect), "RemoveBorderEffect")]
+namespace STOCI.App.iOS
+{
+
+    public class RemoveBorderEffect : PlatformEffect
+    {
+        UISearchBarStyle oldstyle;
+        protected override void OnAttached()
+        {
+            var searchbar = Control as UISearchBar;
+            if (searchbar != null)
+            {
+                oldstyle = searchbar.SearchBarStyle;
+                searchbar.BarStyle = UIBarStyle.Default;
+                searchbar.BackgroundImage = new UIImage();
+                Foundation.NSString _searchField = new Foundation.NSString("searchField");
+                var textFieldInsideSearchBar = (UITextField)searchbar.ValueForKey(_searchField);
+                 textFieldInsideSearchBar.BackgroundColor = UIColor.FromRGB(220, 220, 220);
+
+            }
+
+        }
+
+        protected override void OnDetached()
+        {
+            var searchbar = Control as UISearchBar;
+            if(searchbar!=null)
+            searchbar.SearchBarStyle = oldstyle;
+        }
+    }
+}
